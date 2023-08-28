7384382bf0af005b565f3c12be06b3a2b557b4d1
Edrey Pomarez <65086931+epomarez@users.noreply.github.com>
2023-08-28 13:50:16 -0600

    Add basic card' book structure and functionality

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, summary, pageNumber, isRead) {
+  title,
+    author,
+    summary,
+    pageNumber,
+    isRead
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
 
+const submitBtn = document.getElementById('submit_btn');
+
+
 
 let cardSection = document.querySelector('.cards');
-function addCard(card){
-    cardSection.appendChild(card());
+
+function generateBookFields() {
+  let title = document.createElement('p');
+  title.innerText = "Title: ";
+
+  let author = document.createElement('p');
+  author.innerText = "Author: ";
+
+  let summary = document.createElement('p');
+  summary.innerText = "Summary: ";
+
+  let pagesNumber = document.createElement('p');
+  pagesNumber.innerText = "Pages Number: ";
+
+  let isRead = document.createElement('p');
+  isRead.innerText = "Is read: ";
+
+  return new Array(title, author, summary, pagesNumber, isRead);
 }
 
-for(let i = 0; i<7; i++){
-    addCard(() => {
-        let newCard = document.createElement('div');
-        newCard.style.cssText= 'background-color: #568259; height:200px; width:180px';
-        return newCard});
+function appendBookData(card, bookFields) {
+
+  for (i = 0; i < bookFields.length; i++) {
+    card.appendChild(bookFields[i]);
+  }
+
+  return card;
+}
+
+function addCard(card) {
+  cardSection.appendChild(appendBookData(card(), generateBookFields()));
+}
+
+for (let i = 0; i < 7; i++) {
+  addCard(() => {
+    console.log("Hola");
+    let newCard = document.createElement('div');
+    newCard.style.cssText = 'background-color: #568259; height:200px; width:180px; padding:20px; display:flex; flex-direction:column; row-gap:10px;';
+    return newCard
+  });
 }
